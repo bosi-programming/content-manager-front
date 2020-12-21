59ab1e9d5dfdf41afe1035b0ba6a11bfba5bf4d0
Felipe Azevedo Bosi <felipeb@lumahealth.io>
2020-12-21 18:01:45 -0300

    Add MUI and media-queries for editor

--- src/screens/EditorScreen.js
+++ src/screens/EditorScreen.js
@@ -1,11 +1,42 @@
 import React, { useState } from "react";
+import classnames from "classnames";
+import { makeStyles } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
-import PostEditor from '../components/PostEditor';
+import PostEditor from "../components/PostEditor";
 
+const useStyles = makeStyles({
+  editorMob: {
+    margin: "0 20px",
+    width: "100%",
+  },
+  editorTablet: {
+    margin: "0 auto",
+    maxWidth: 900,
+  },
+  editorDesktop: {
+    margin: "0 auto",
+    maxWidth: 1200,
+  },
+});
 const EditorScreen = () => {
+  const classes = useStyles();
   const [postState, setPostState] = useState("");
+  const mobile = useMediaQuery("(max-width: 768px)");
+  const tablet = useMediaQuery("(min-width: 769px) and (max-width: 1200px)");
+  const desktop = useMediaQuery("(min-width: 1201px)");
 
-  return <PostEditor postState={postState} setPostState={setPostState} />;
+  return (
+    <div
+      className={classnames({
+        [classes.editorMob]: mobile,
+        [classes.editorTablet]: tablet,
+        [classes.editorDesktop]: desktop,
+      })}
+    >
+      <PostEditor postState={postState} setPostState={setPostState} />
+    </div>
+  );
 };
 
 export default EditorScreen;
