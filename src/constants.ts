638ec3c164d613d130557613aee97ebb08dbd9b9
Felipe A.Bosi <bosibackend@protonmail.com>
2021-06-12 17:13:01 -0300

    Change localhost backend port

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 const isDevelopment = /localhost/.test(window.location.toString());
 
 const constants = {
-  baseUrl: isDevelopment ? "http://localhost:3000/api" : "https://content-manager-backend.herokuapp.com/api",
+  baseUrl: isDevelopment ? "http://localhost:8002/api" : "https://content-manager-backend.herokuapp.com/api",
 };
 
 export default constants;
